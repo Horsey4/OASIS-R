7fd7324809662b0f3ab64f253601aabea38418c8
Horsey4 <62522547+Horsey4@users.noreply.github.com>
2024-02-09 15:26:37 -0600

    Slim down CursorGUI

---
+++ CursorGUI.cs
@@ -0,0 +1,42 @@
+﻿using HutongGames.PlayMaker;
+
+namespace OASIS;
+
+public static class CursorGUI
+{
+    private static readonly FsmBool guiAssemble = FsmVariables.GlobalVariables.FindFsmBool("GUIassemble");
+    private static readonly FsmBool guiBuy = FsmVariables.GlobalVariables.FindFsmBool("GUIbuy");
+    private static readonly FsmBool guiDisassemble = FsmVariables.GlobalVariables.FindFsmBool("GUIdisassemble");
+    private static readonly FsmBool guiUse = FsmVariables.GlobalVariables.FindFsmBool("GUIuse");
+    private static readonly FsmString guiInteraction = FsmVariables.GlobalVariables.FindFsmString("GUIinteraction");
+
+    public static bool Assemble
+    {
+        get => guiAssemble.Value;
+        set => guiAssemble.Value = value;
+    }
+
+    public static bool Buy
+    {
+        get => guiBuy.Value;
+        set => guiBuy.Value = value;
+    }
+
+    public static bool Disassemble
+    {
+        get => guiDisassemble.Value;
+        set => guiDisassemble.Value = value;
+    }
+
+    public static bool Use
+    {
+        get => guiUse.Value;
+        set => guiUse.Value = value;
+    }
+
+    public static string Interaction
+    {
+        get => guiInteraction.Value;
+        set => guiInteraction.Value = value;
+    }
+}
