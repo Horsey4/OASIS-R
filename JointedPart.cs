29c31da03df053b61f25f55a008496802a7e9eff
Horsey4 <62522547+Horsey4@users.noreply.github.com>
2024-02-11 20:11:54 -0600

    Add JointedPart base class

---
+++ JointedPart.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace OASIS;
+
+public delegate void JointBrokenCallback(int triggerIndex, float breakForce);
+
+public abstract class JointedPart<TJoint> : Attachable
+    where TJoint : Joint
+{
+    public event JointBrokenCallback OnJointBroken;
+    public bool invokeOnDetachWhenBroken = true;
+
+    public TJoint Joint { get; private set; }
+
+    public override void Attach(int triggerIndex, bool silent, bool notify)
+    {
+        base.Attach(triggerIndex, silent, notify);
+
+        Joint = gameObject.AddComponent<TJoint>();
+        Joint.connectedBody = transform.parent.GetComponentInParent<Rigidbody>();
+    }
+
+    public override void Detach(bool silent, bool notify)
+    {
+        base.Detach(silent, notify);
+
+        Destroy(Joint);
+    }
+
+    protected virtual void OnJointBreak(float breakForce)
+    {
+        OnJointBroken?.Invoke(AttachedToIndex, breakForce);
+        base.Detach(false, invokeOnDetachWhenBroken);
+    }
+}
