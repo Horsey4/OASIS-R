1670dbcf955291c5bf7553e9b747d6cef46d5eff
Horsey4 <62522547+Horsey4@users.noreply.github.com>
2024-02-18 17:07:18 -0600

    Add HandScrew

---
+++ HandScrew.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace OASIS;
+
+public class HandScrew : Screwable
+{
+    public float screwCooldown = 0.1f;
+
+    protected override void OnCursorOver()
+    {
+        CursorGUI.Use = true;
+        if (Input.mouseScrollDelta.y == 0) return;
+
+        Screw(Math.Sign(Input.mouseScrollDelta.y), screwCooldown);
+    }
+
+    protected override void OnCursorExit() => CursorGUI.Use = false;
+}
