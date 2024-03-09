f660cbe0329767676606741c2cf31f787f40c6b1
Horsey4 <62522547+Horsey4@users.noreply.github.com>
2024-03-08 18:26:25 -0600

    Document Interactable

--- Interactable.cs
+++ Interactable.cs
@@ -2,6 +2,9 @@
 
 namespace OASIS;
 
+/// <summary>
+/// Provides functionality for raycast-based interaction
+/// </summary>
 [RequireComponent(typeof(Collider))]
 public abstract class Interactable : MonoBehaviour
 {
@@ -9,12 +12,24 @@ public abstract class Interactable : MonoBehaviour
     public float maxInteractionDistance = 1;
     private protected static Transform playerCamera;
 
+    /// <summary>
+    /// Returns <see langword="true"></see> every frame <see cref="OnCursorOver"/> is called
+    /// </summary>
     public bool IsCursorOver { get; private set; }
 
+    /// <summary>
+    /// Called on the first frame the raycast hits an attached collider
+    /// </summary>
     protected virtual void OnCursorEnter() { }
 
+    /// <summary>
+    /// Called every frame the raycast hits an attached collider
+    /// </summary>
     protected virtual void OnCursorOver() { }
 
+    /// <summary>
+    /// Called on the first frame the raycast no longer hits an attached collider
+    /// </summary>
     protected virtual void OnCursorExit() { }
 
     protected virtual void Awake()
