7f4c3d00ed847e63c803ccd896aeda10097db651
Horsey4 <62522547+Horsey4@users.noreply.github.com>
2024-03-10 18:28:30 -0500

    Document Fastener

--- Fastener.cs
+++ Fastener.cs
@@ -4,13 +4,24 @@ namespace OASIS;
 
 public delegate void TightnessChangedCallback(int deltaTightness);
 
+/// <summary>
+/// Provides a common interface for securing <see cref="Attachable"/> components
+/// </summary>
 public abstract class Fastener : Interactable
 {
     public event TightnessChangedCallback OnTightnessChanged;
     public int maxTightness;
 
+    /// <summary>
+    /// The current tightness stage
+    /// </summary>
     public int Tightness { get; private set; }
 
+    /// <summary>
+    /// Sets the tightness stage to <paramref name="value"/>
+    /// </summary>
+    /// <param name="value">The new tightness stage</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
     public virtual void SetTightness(int value)
     {
         if (value < 0 || value > maxTightness) throw new ArgumentOutOfRangeException(nameof(value),
